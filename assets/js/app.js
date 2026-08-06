2f96fd6f03617433d85217dc63d5158f88e729b5
lazartegonzalo09-ux <lazartegonzalo09@gmail.com>
2026-08-06 15:49:46 -0300

    Update app.js
    Arreglo submenus internos

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,11 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const botones = document.querySelectorAll(".dropdown-btn");
 
-
     botones.forEach(function(boton){
 
         boton.addEventListener("click", function(){
 
-            const submenu = this.nextElementSibling;
-
+            const submenu = boton.parentElement.querySelector(":scope > .submenu");
 
             if(submenu){
 
@@ -27,6 +25,5 @@ document.addEventListener("DOMContentLoaded", function(){
         });
 
     });
-
 
 });
